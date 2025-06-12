15762811976795f9b84fabcb2a61cae489140698
ALEXXO078 <deabas78@gmail.com>
2025-06-12 12:41:04 -0500

    Eliminar archivos restantes del repositorio

--- 6. Pirámide selectiva.py
+++
@@ -1,7 +0,0 @@
-n=3
-o=3
-for i in range(1,n+1):
-    for a in range(i):
-     print(o,end=" ")
-     o += 3
-    print()
--- 8. Conteo de vocales específicas.py
+++
@@ -1,9 +0,0 @@
-pal=input("escriba una frase: ")
-let="e"
-can=0
-for a in pal:
-    if a=="e":
-       can+=1
-print("la cantidad de veces que aparece la e es de: ",can)
-if can>5:
-    print("Hay muchas e!!!") 
